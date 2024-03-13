01dc1e834cd8c85ef5d5443df4f5b14ea8bdea9b
Felipe Machado Maciel <ART0055483@aluno.firjansenaisesi.com.br>
2024-03-13 11:06:28 -0300

    opa, esqueci de ajeitar o index dps do conflito de commits

--- index.js
+++ index.js
@@ -1,88 +1,43 @@
-<<<<<<< HEAD
-import inquirer from "inquirer";
-import getGrades  from "./scrapers/grades.js";
-import getPresence from "./scrapers/presence.js";
+import inquirer from "inquirer"
+import getGrades from "./scrapers/grades.js"
+import getPresence from "./scrapers/presence.js"
 
 async function main() {
-    console.log("\nBem-vindo ao scraper da firjan!")
-    while (true) {
-        const {value} = await inquirer.prompt({
-            message: "O que deseja fazer?",
-            name: "value",
-            type: "list",
-            choices: [
-                {
-                    name: "Pegar notas",
-                    value: 0
-                },
-                {
-                    name: "Pegar percentual de presenças",
-                    value: 1
-                },
-                {
-                    name: "Sair",
-                    value: 2
-                }
-            ]
-        })
-    
-        if (value == 0) {
-            const grades = await getGrades()
-            console.log(JSON.stringify(grades, null, 2) + "\n")
-        }
-        if (value == 1) {
-            const presences = await getPresence()
-            console.log(JSON.stringify(presences, null, 2) + "\n")
-        }
-        if (value >= 2) {
-            break
-        }
-        continue
-    }
+	console.log("\nBem-vindo ao scraper da firjan!")
+	while (true) {
+		const { value } = await inquirer.prompt({
+			message: "O que deseja fazer?",
+			name: "value",
+			type: "list",
+			choices: [
+				{
+					name: "Pegar notas",
+					value: 0,
+				},
+				{
+					name: "Pegar percentual de presenças",
+					value: 1,
+				},
+				{
+					name: "Sair",
+					value: 2,
+				},
+			],
+		})
+
+		if (value == 0) {
+			const grades = await getGrades()
+			console.log(JSON.stringify(grades, null, 2) + "\n")
+		}
+		if (value == 1) {
+			const presences = await getPresence()
+			console.log(JSON.stringify(presences, null, 2) + "\n")
+		}
+		if (value >= 2) {
+			break
+		}
+		continue
+	}
 }
 
-=======
-import inquirer from "inquirer";
-import getGrades  from "./scrapers/grades.js";
-import getPresence from "./scrapers/presence.js";
-
-async function main() {
-    console.log("\nBem-vindo ao scraper da firjan!")
-    while (true) {
-        const {value} = await inquirer.prompt({
-            message: "O que deseja fazer?",
-            name: "value",
-            type: "list",
-            choices: [
-                {
-                    name: "Pegar notas",
-                    value: 0
-                },
-                {
-                    name: "Pegar percentual de presenças",
-                    value: 1
-                },
-                {
-                    name: "Sair",
-                    value: 2
-                }
-            ]
-        })
-    
-        if (value == 0) {
-            const grades = await getGrades()
-            console.log(JSON.stringify(grades, null, 2) + "\n")
-        }
-        if (value == 1) {
-            const presences = await getPresence()
-            console.log(JSON.stringify(presences, null, 2) + "\n")
-        }
-        if (value >= 2) {
-            break
-        }
-        continue
-    }
-}
-
->>>>>>> 3153fd737d49e48b92ff06f9437c3c25ba1ae123
 main()
